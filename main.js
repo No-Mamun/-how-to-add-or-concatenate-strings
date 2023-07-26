807579bb4a27f8aa7ae6eb26d4bcc6282da941b1
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-07-27 00:10:29 +0600

    Add files via upload
     how to add or concatenate strings

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// how to add or concatenate strings
+
+/*
+note:1
+document.write("Mamun" + " Khan" + " Isma" + " Benjer");
+
+*/
+
+/*
+Node:2
+
+var firstName = "Mamun";
+
+var lastName = " Khan";
+
+var contary = " Bangladesh";
+
+var vallage = " Purbopra";
+
+var fullName = firstName + lastName + contary + vallage;
+
+document.write(fullName);
+
+*/
+// Node:3
+
+var fullName = ' firstName + lastName + contary + vallage';
+
+document.write("All How Are You" + fullName +" Than You");
